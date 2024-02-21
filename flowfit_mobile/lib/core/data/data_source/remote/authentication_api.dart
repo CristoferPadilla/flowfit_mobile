916e1264694e0427bf100374b3257a764f813399
CristoferPadilla <149548191+CristoferPadilla@users.noreply.github.com>
2024-02-20 18:38:55 -0600

    NADA NUEVO

--- flowfit_mobile/lib/core/data/data_source/remote/authentication_api.dart
+++ flowfit_mobile/lib/core/data/data_source/remote/authentication_api.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flowfit_mobile/core/data/helpers/http.dart';
 import 'package:flowfit_mobile/core/data/helpers/http_method.dart';
 import 'package:flowfit_mobile/core/domain/responses/login_response.dart';
@@ -15,6 +17,11 @@ class AuthenticationAPI {
         method: HttpMethod.post,
         timeOut: const Duration(seconds: 10),
       );
+
+      print("result data ${result.data}");
+      print("result data runtimeTupe ${result.data.runtimeType}");
+      print("result error data ${result.error?.data}");
+      print("result error ${result.statusCode}");
 
       if (result.statusCode == 200) {
         return LoginResponse.ok;
