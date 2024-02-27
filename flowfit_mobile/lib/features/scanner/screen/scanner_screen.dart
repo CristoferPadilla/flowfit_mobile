00454c559068ebb0c4251a30baac159408308f1c
CristoferPadilla <149548191+CristoferPadilla@users.noreply.github.com>
2024-02-26 20:49:30 -0600

    escanea

--- flowfit_mobile/lib/features/scanner/screen/scanner_screen.dart
+++ flowfit_mobile/lib/features/scanner/screen/scanner_screen.dart
@@ -1,10 +1,10 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:flowfit_mobile/resources/themes/font_styles.dart';
-import 'package:flowfit_mobile/resources/themes/primary_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MobileScannerScreen extends StatefulWidget {
   const MobileScannerScreen({Key? key}) : super(key: key);
@@ -28,7 +28,7 @@ class _MobileScannerScreenState extends State<MobileScannerScreen> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title:  Text('Registrar entrada', style: FontStyle.titleTextStyle,),
+        title: const Text('Registrar entrada'),
         actions: [
           IconButton(
             color: Colors.white,
@@ -49,12 +49,21 @@ class _MobileScannerScreenState extends State<MobileScannerScreen> {
         ],
       ),
       body: MobileScanner(
+
         controller: cameraController,
-        onDetect: (capture) {
+        onDetect: (capture) async {
           final List<Barcode> barcodes = capture.barcodes;
-          final Uint8List? image = capture.image;
           for (final barcode in barcodes) {
             debugPrint('Barcode found! ${barcode.rawValue}');
+            final endpoint = barcode.rawValue;
+            if (endpoint != null) {
+              final success = await sendDataToAPI(endpoint);
+              if (success) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Datos enviados correctamente')),
+                ); 
+              }
+            }
           }
         },
       ),
@@ -68,11 +77,46 @@ class _MobileScannerScreenState extends State<MobileScannerScreen> {
               case TorchState.off:
                 return const Icon(Icons.flash_off, color: Colors.grey);
               case TorchState.on:
-                return const Icon(Icons.flash_on, color: PrimaryTheme.secundaryColor);
+                return const Icon(Icons.flash_on, color: Colors.blue);
             }
           },
         ),
       ),
     );
+  }
+
+  Future<bool> sendDataToAPI(String endpoint) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final accessToken = prefs.getString('accessToken');
+
+      if (accessToken != null) {
+        debugPrint('Endpoint: $endpoint');
+        debugPrint('AccessToken: $accessToken');
+
+        final uri = Uri.parse(endpoint); 
+        final response = await http.post(
+          uri, 
+          headers: {
+            'Authorization': 'Bearer $accessToken',
+            'Content-Type': 'application/json',
+          },
+        );
+
+        if (response.statusCode == 201) {
+          print(response.body);
+          return true;
+        } else {
+          debugPrint('Error en la solicitud: ${response.statusCode}');
+          return false;
+        }
+      } else {
+        debugPrint('AccessToken o Id no encontrados en SharedPreferences');
+        return false;
+      }
+    } catch (e) {
+      debugPrint('Error al enviar los datos: $e');
+      return false;
+    }
   }
 }
