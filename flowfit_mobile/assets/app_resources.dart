ca52eeb8ad3081e6bca885c182c9613addc54a43
CristoferPadilla <149548191+CristoferPadilla@users.noreply.github.com>
2024-03-11 15:36:03 -0600

    loadingScreen

--- flowfit_mobile/assets/app_resources.dart
+++ flowfit_mobile/assets/app_resources.dart
@@ -1,9 +1,13 @@
 
 class AppResource{
-
+  final String profileImage = 'assets/jpg/profile_example.jpeg';
   final String ejercicio1 = 'assets/jpeg/Ejercicio1.jpeg';
   final String ejercicio2 = 'assets/jpeg/Ejercicio2.jpeg';
   final String ejercicio3 = 'assets/jpeg/Ejercicio3.jpeg';
-  final String profileImage = 'assets/jpg/profile_example.jpeg';
   final String shopImage = 'assets/jpge/protein.jpeg';
+
+  //svg
+  final String logoSvg = 'assets/svg/logo.svg';
+
+
 }
